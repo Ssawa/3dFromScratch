3c142b1b41eed958d3926301dac646d4a7d9ff28
CJ DiMaggio <ssawaa@yahoo.com>
2015-01-10 13:53:47 -0500

    Refactor scalable 2d arrays and add/subtract matrixes function

--- src/3dTest.c
+++ src/3dTest.c
@@ -7,52 +7,65 @@ typedef struct vector3d {
 	int z;
 } Vector3d;
 
-
-// TODO - MOVE INTO A SEPERATE, ENCAPSULATED FILE
+// TODO - See if there's an easier way to handle scalable 2d arrays
+// preferably something not involving macro magic 
 typedef struct matrix {
-	int rows;
-	int columns;
+	unsigned int rows;
+	unsigned int columns;
 	int* array;
-
-	void (*delete) (struct matrix*);
-	int (*get) (struct matrix*, unsigned x, unsigned y);
 } Matrix;
 
-void deleteMatrix(Matrix* self);
-int getMatrixElement(Matrix* self, unsigned x, unsigned y);
+int getIndex(int width, int x, int y) {
+	return (width * x + y);
+}
 
-// Not as pretty as a multidimensional array but it is functionally identical and gives
-// us the benefit of having arbitrarily sized matrixes, and is much cleaner than a jagged
-// array, in my mind at least. As this is a simple, purposfully limited, test library
-// we could probably get away with defining a few 
-Matrix* newMatrix(int rows, int columns, int array[rows][columns]) {
-	int* flatArray = malloc(sizeof(int[rows * columns]));
-	for (int x = 0; x < rows; x++) {
-		for (int y = 0; y < columns; y++) {
-			flatArray[rows * x + y] = array[x][y];
+typedef enum {ADD, SUBTRACT} AddOrSubtract;
+void addOrSubtracMatrixes(AddOrSubtract addOrSubtract, Matrix matrix1, Matrix matrix2, Matrix *matrixSum) {
+
+	// To add matrixes they must be the same dimensions
+	if (matrix1.rows != matrix2.rows || matrix1.columns != matrix2.columns) {
+		return;
+	}
+
+	// This could just as easily have been initialized with matrix2, as we should have
+	// already verified that they are the same dimensions.
+	int sumRows = matrix1.rows;
+	int sumColumns = matrix1.columns;
+	matrixSum->rows = sumRows;
+	matrixSum->columns = sumColumns;
+
+	for (int x = 0; x < sumRows; x++) {
+		for (int y = 0; y < sumColumns; y++) {
+			int index = getIndex(sumColumns, x, y);
+			switch (addOrSubtract) {
+				case ADD: {
+					matrixSum->array[index] = matrix1.array[index] + matrix2.array[index];
+					break;
+				}
+				case SUBTRACT: {
+					matrixSum->array[index] = matrix1.array[index] - matrix2.array[index];
+					break;
+				}
+			}
 		}
 	}
-	Matrix* matrix = malloc(sizeof(Matrix));
-	matrix->rows = rows;
-	matrix->columns = columns;
-	matrix->array = flatArray;
+}
 
-	matrix->delete = &deleteMatrix;
-	matrix->get = &getMatrixElement;
-	return matrix;
-}
-void deleteMatrix(Matrix* self) {
-	free(self->array);
-	free(self);
-}
-int getMatrixElement(Matrix* self, unsigned int x, unsigned int y) {
-	if (x > self->rows-1 || y > self->columns-1) {
-		// Okay, so this might not be the safest thing to do to indicate an error
-		// but for debugging purposes it should be sufficiant at indicating to me that
-		// I screwed something up
-		return -1000000;
+void printMatrix(Matrix matrix) {
+	int rw = matrix.rows;
+	int clm = matrix.columns;
+
+	for (int x = 0; x < rw; x++) {
+		for (int y = 0; y < clm; y++) {
+			printf("%d", matrix.array[getIndex(clm, x, y)]);
+
+			if ( y == clm - 1) {
+				printf("\n");
+			} else {
+				printf(" | ");
+			}
+		}
 	}
-	return self->array[self->rows * x + y];
 }
 
 typedef struct camera {
@@ -81,12 +94,19 @@ int main() {
 		{1, -1, -1}
 	};
 
-	int twod[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-	Matrix* testMatrix = newMatrix(3, 3, twod);
+	int arrayTest[5][5] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+	Matrix matrixTest = {5, 5, (int*) arrayTest};
+	printMatrix(matrixTest);
+
+	int arrayBlank[5][5] = {};
+	Matrix sumMat = {0, 0, (int*) arrayBlank};
+	addOrSubtracMatrixes(ADD, matrixTest, matrixTest, &sumMat);
+
+	printMatrix(sumMat);
 
 	Cube testCube = {"TestCube", verticeArray, testVector, testVector};
 	printf("%s\n", testCube.name);
 	printf("%d\n", testCube.vertices[1].x);
-	printf("%d\n", testMatrix->get(testMatrix, 1, 3));
-	testMatrix->delete(testMatrix);
+
+	return 0;
 }
